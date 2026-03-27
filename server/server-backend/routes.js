93aaf753a6533a89cbcf5aa3543132b68e9d1d1f
recursivetree <recursivetreemail@gmail.com>
2026-03-27 23:01:14 +0100

    classroom management APIs

--- server/server-backend/routes.js
+++ server/server-backend/routes.js
@@ -14,14 +14,61 @@ router.post("/classroom/create",async (req, res)=>{
         return errorResponse(req, 400,"missing classroomName (string)")
     }
     if(classroomName.length < 1) {
-        return errorResponse(req, 400,"classroomName (string) is too short")
+        return errorResponse(res, 400,"classroomName (string) is too short")
+    }
+    if(classroomName.length > 255) {
+        return errorResponse(res, 400,"classroomName (string) is too long")
     }
 
-    const [results, fields] = await req.db.execute("INSERT INTO classrooms VALUES (?)",[classroomName])
-
-    console.log(results, fields)
+    const [results, fields] = await req.db.execute("INSERT INTO classrooms (name) VALUES (?)",[classroomName])
 
     return res.json({
-
+        classroomID: results.insertId
     })
+})
+
+router.get("/classrooms", async (req, res) => {
+    const [rows, fields] = await req.db.execute("SELECT * FROM classrooms")
+
+    await res.json(rows.map(e=>{return {classroomId: e.id, classroomName: e.name}}))
+})
+
+router.post("/classroom/update",async (req, res)=> {
+    const classroomId = req.body.classroomId
+    const classroomName = req.body.classroomName
+
+    if(typeof classroomId !== "number") {
+        return errorResponse(req, 400,"missing classroomId (int)")
+    }
+    if(classroomId < 0) {
+        return errorResponse(res, 400,"classroomId (int) must be positive")
+    }
+    if(typeof classroomName !== "string") {
+        return errorResponse(req, 400,"missing classroomName (string)")
+    }
+    if(classroomName.length < 1) {
+        return errorResponse(res, 400,"classroomName (string) is too short")
+    }
+    if(classroomName.length > 255) {
+        return errorResponse(res, 400,"classroomName (string) is too long")
+    }
+
+    const [results, fields] = await req.db.execute("UPDATE classrooms SET name=? where id=?",[classroomName, classroomId])
+
+    return res.json({})
+})
+
+router.post("/classroom/delete",async (req, res)=>{
+    const classroomId = req.body.classroomId
+
+    if(typeof classroomId !== "number") {
+        return errorResponse(req, 400,"missing classroomId (int)")
+    }
+    if(classroomId < 0) {
+        return errorResponse(res, 400,"classroomId (int) must be positive")
+    }
+
+    const [results, fields] = await req.db.execute("DELETE FROM classrooms where id=?",[classroomId])
+
+    return res.json({})
 })
