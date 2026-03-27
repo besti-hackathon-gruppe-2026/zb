2d18c844677a78cbab790dd46505f7453ad64dbd
Tim-foe <118597877+Tim-foe@users.noreply.github.com>
2026-03-27 20:04:45 +0100

    navbar

---
+++ frontend/src/lib/components/Navbar.svelte
@@ -0,0 +1,50 @@
+<script>
+
+	import { LogOut, Users, SquarePen, CalendarDays, House } from "@lucide/svelte";
+
+</script>
+
+
+
+<nav class="taskbar">
+  <ul class="taskbar-list">
+    <li><a href="home"><p>Home</p></a></li>
+    <li><p>Stundenplan</p></li>
+    <li><p>Filter</p></li>
+    <li><p>Klassenzimmer</p></li>
+  </ul>
+  <div class="logout-icon"><LogOut/></div>
+  
+</nav>
+
+<style>
+
+.taskbar {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  border-bottom: 2px solid black;
+  padding: 1rem 2rem;
+  box-sizing: border-box;
+}
+
+.taskbar-list {
+  display: flex;
+  gap: 5rem;
+  list-style-type: none;
+  align-items: center;
+  margin: 0;
+  flex: 1;
+}
+
+.logout-icon {
+  margin-left: auto; 
+  flex-shrink: 0;
+}
+
+.taskbar-list li:hover p, .logout-icon:hover {
+  color: grey;
+  cursor: pointer;
+}
+
+ </style> 
