67ab92db0dfbe662581e5d8431d6d8104c6effc4
KingSzobo08 <otto.dorgovics@outlook.com>
2026-03-27 16:29:04 +0100

    Starterpackage mit Create funktion

---
+++ frontend/src/routes/starterpackage/create.svelte
@@ -0,0 +1,15 @@
+<script>
+    let subject = $state("");
+    
+    function handleConfirm() {
+        console.log(subject);       //test
+        
+  }
+
+</script>
+
+<div>
+  <p>Subject</p>
+  <input type="text" placeholder="Enter subject" bind:value={subject} />
+  <button onclick={() => handleConfirm()}>Confirm</button>
+</div>
