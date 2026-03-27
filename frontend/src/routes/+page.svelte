7d18dc7aab53c6ca17ca7312722571aa44690297
Yaron Traub <y.traub@outlook.com>
2026-03-27 14:33:44 +0100

    Added lucide

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,8 @@
+<script>
+  import Camera from '@lucide/svelte/icons/camera';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<Camera />
