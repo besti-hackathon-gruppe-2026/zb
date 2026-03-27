897372a5cb28b120718eee4da2f95f2c880e8903
Yaron Traub <y.traub@outlook.com>
2026-03-27 16:39:21 +0100

    Added login functionality

---
+++ frontend/src/routes/login/+page.server.ts
@@ -0,0 +1,13 @@
+import type { Actions } from './$types';
+
+export const actions = {
+	login: async ({ cookies, request }) => {
+		const data = await request.formData();
+		const email = data.get('email');
+		const password = data.get('password');
+
+		console.log(email, password)
+
+		return { success: true };
+	}
+} satisfies Actions;
--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -2,15 +2,21 @@
 	<section class="login-card container">
 		<h1 class="title">Login</h1>
 
-		<form method="post">
+		<form method="POST" action="?/login">
 			<div class="form-field">
-				<label for="username">Username</label>
-				<input id="username" name="username" type="text" required autocomplete="username" />
+				<label for="email">Email</label>
+				<input id="email" name="email" type="text" required autocomplete="email" />
 			</div>
 
 			<div class="form-field">
 				<label for="password">Password</label>
-				<input id="password" name="password" type="password" required autocomplete="current-password" />
+				<input
+					id="password"
+					name="password"
+					type="password"
+					required
+					autocomplete="current-password"
+				/>
 			</div>
 
 			<button type="submit">Login</button>
@@ -19,30 +25,30 @@
 </main>
 
 <style>
-.title {
-  text-align: center;
-}
+	.title {
+		text-align: center;
+	}
 
-.login-card {
-	display: flex;
-	flex-direction: column;
-	width: 100%;
+	.login-card {
+		display: flex;
+		flex-direction: column;
+		width: 100%;
 
-	max-width: 420px;
+		max-width: 420px;
 
-	padding: 2.5rem;
-	gap: 1.5rem;
-}
+		padding: 2.5rem;
+		gap: 1.5rem;
+	}
 
-.form-field {
-	display: flex;
-	flex-direction: column;
-	gap: 0.25rem;
-}
+	.form-field {
+		display: flex;
+		flex-direction: column;
+		gap: 0.25rem;
+	}
 
-form {
-	display: flex;
-	flex-direction: column;
-	gap: 1.25rem;
-}
+	form {
+		display: flex;
+		flex-direction: column;
+		gap: 1.25rem;
+	}
 </style>
