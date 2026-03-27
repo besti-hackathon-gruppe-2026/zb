59449182cbdde10440f7103563a16f6262d9dcce
Yaron Traub <y.traub@outlook.com>
2026-03-27 13:10:23 +0100

    Sveltekit setup

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<p>Navbar</p>
+
+<div>
+	{@render children()}
+</div>
+
